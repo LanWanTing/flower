9ba2b5cdd65cba4c55942b248642b15c80090b22
LanWanTing <34399294+LanWanTing@users.noreply.github.com>
2018-11-19 00:31:25 +0800

    1119
    1119

---
+++ js/common.js
@@ -0,0 +1,14 @@
+﻿jQuery(document).ready(function(e) {
+  // Menu    
+	jQuery('.iMenu li.menuLi').hover(function(e) {
+		var $cEle = jQuery(this);
+		$cEle.find('li.menuLi a.link').addClass('');    
+		$cEle.children('div.navSub').stop(false, true).fadeIn(300);
+	}, function(e) {
+		var $cEle = jQuery(this);
+		$cEle.find('li.menuLi a.link').removeClass('');  
+		$cEle.children('div.navSub').stop(false, true).fadeOut(300);  
+	});
+});
+
+
